87259ef8a438c5f12ecd2555677b3662a579beb9
Pratik Poudel <iampratik32@gmail.com>
2022-12-02 15:51:26 +0545

    Storage Access

--- index.js
+++ index.js
@@ -18,11 +18,12 @@ app.use(
         // crossOriginResourcePolicy: { policy: 'cross-origin' }
     })
 )
+
 app.use(cors())
 app.use(express.urlencoded({ limit: '1mb', extended: true }));
 app.use(methodOverride('_method'))
 app.use(express.json({ limit: '1mb' }))
-app.use(express.static(path.join(__dirname, './src/public')))
+app.use(express.static(path.join(__dirname, './src/storage')))
 
 app.use('/api/v1/admin', adminApi())
 app.use('/api/v1', userApi())
