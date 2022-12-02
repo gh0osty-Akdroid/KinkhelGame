0f4a3da9b5fd59d3c74f41d84ceecf96bf6c9d20
Pratik Poudel <iampratik32@gmail.com>
2022-12-02 16:09:45 +0545

    Images Fix

--- src/utils/fileHandler.js
+++ src/utils/fileHandler.js
@@ -2,16 +2,21 @@ const randomstring = require('randomstring')
 const fs = require('fs')
 
 exports.addImage = async (image) => {
-    const location = 'src/storage/uploads'
-    const filename = Date.now() + randomstring.generate('8')
-    var data = image.replace(/^data:image\/\w+;base64,/, "")
-    var fName = `${location}/${filename}.png`
-    var dbLocation = `uploads/${filename}.png`
-    fs.writeFileSync(fName, data, { encoding: "base64" }, function (err) {
-        responses.serverError(res, err)
-    })
+    try {
+        const location = 'src/storage/uploads'
+        const filename = Date.now() + randomstring.generate('8')
+        var data = image.replace(/^data:image\/\w+;base64,/, "")
+        var fName = `${location}/${filename}.png`
+        var dbLocation = `uploads/${filename}.png`
+        fs.writeFileSync(fName, data, { encoding: "base64" }, function (err) {
+            responses.serverError(res, err)
+        })
 
-    return dbLocation
+        return dbLocation
+    }
+    catch (err) {
+        return err.message
+    }
 }
 
 exports.removeImage = async (url) => {
